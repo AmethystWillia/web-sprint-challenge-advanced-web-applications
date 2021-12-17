2f73a2099f24f0265ba3ccc3ea48482b5b6e1588
Amethyst Willia <zmithic@gmail.com>
2021-12-17 12:35:09 -0600

    Set up login, successfully takes user info and displays View

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,13 +1,67 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useState } from 'react';
+import { useHistory } from 'react-router-dom';
 import styled from 'styled-components';
 
 const Login = () => {
+    const [state, setState] = useState({
+        credentials: {
+            username: '',
+            password: '',
+        }
+    });
+    const { push } = useHistory();
+
+    const handleChange = e => {
+        setState ({
+            credentials: {
+                ...state.credentials,
+                [e.target.name]: e.target.value,
+            }
+        });
+    };
+
+    const login = e => {
+        e.preventDefault();
+
+        axios.post('http://localhost:5000/api/login', state.credentials)
+            .then(resp => {
+                const { token, role, username } = resp.data;
+                localStorage.setItem('token', token);
+                localStorage.setItem('role', role);
+                localStorage.setItem('username', username);
+                push('/view');
+            })
+            .catch(err => {
+                console.error(err);
+            })
+    };
     
     return(<ComponentContainer>
         <ModalContainer>
             <h1>Welcome to Blogger Pro</h1>
             <h2>Please enter your account information.</h2>
         </ModalContainer>
+        <FormGroup>
+            <Label>Username</Label>
+            <Input
+                type='text'
+                name='username'
+                id='username'
+                value={state.credentials.username}
+                onChange={handleChange}
+            />
+            <Label>Password</Label>
+            <Input
+                type='password'
+                name='password'
+                id='password'
+                value={state.credentials.password}
+                onChange={handleChange}
+            />
+            <Button id='submit' onClick={login}>Log In</Button>
+        </FormGroup>
+        <p id='error'>a server provided error message can be found in err.response.data</p>
     </ComponentContainer>);
 }
 
