d2b38385363eeaf458cb84d546f9f8c07438861d
Amethyst Willia <zmithic@gmail.com>
2021-12-17 14:32:42 -0600

    Implemented and passed tests for: rendering Article w/out errors; Rendering headline, author, summary, body when passed through props

--- src/components/Article.test.js
+++ src/components/Article.test.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { screen, render } from '@testing-library/react';
 import '@testing-library/jest-dom';
 
 import userEvent from '@testing-library/user-event';
@@ -6,16 +7,42 @@ import MutationObserver from 'mutationobserver-shim';
 
 import Article from './Article';
 
-test('renders component without errors', ()=> {
+test('renders component without errors', () => {
+    render (<Article article={{}} />);
 });
 
 test('renders headline, author from the article when passed in through props', ()=> {
+    // Arrange
+    render (<Article article={{
+        headline: 'headline test',
+        author: 'author test',
+        summary: 'summary test',
+        body: 'body test',
+    }} />);
+    // Act
+    const headline = screen.queryByTestId('headline');
+    const author = screen.queryByTestId('author');
+    const summary = screen.queryByTestId('summary');
+    const body = screen.queryByTestId('body');
+    // Assert
+    expect(headline).toBeInTheDocument();
+    expect(author).toBeInTheDocument();
+    expect(summary).toBeInTheDocument();
+    expect(body).toBeInTheDocument();
 });
 
 test('renders "Associated Press" when no author is given', ()=> {
+    // Arrange
+    // Act
+    // Assert
+    expect(badVar).toBeInTheDocument();
 });
 
 test('executes handleDelete when the delete button is pressed', ()=> {
+    // Arrange
+    // Act
+    // Assert
+    expect(badVar).toBeInTheDocument();
 });
 
 //Task List: 
